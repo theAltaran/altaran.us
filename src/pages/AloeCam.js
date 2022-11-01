dd1fcce19ce70fc380a0b4bf0a11e7c5424bbeef
Matt Craft <wmcraft1@gmail.com>
2022-11-01 14:06:47 -0400

    aloeCam updates

--- src/pages/AloeCam.js
+++ src/pages/AloeCam.js
@@ -1,5 +1,6 @@
+import "../styles.css"
 export default function AloeCam() {
-  return <h1>AloeCam</h1>
+  return <iframe id="aloeCam" src="http://aloecam.ddns.net:6969/" width={1920} height={1080}></iframe>
   
 
 }
