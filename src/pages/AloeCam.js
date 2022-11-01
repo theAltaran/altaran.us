4e6f961e3c639ab1f420c6f8010e974bad702b91
Matt Craft <wmcraft1@gmail.com>
2022-11-01 15:23:56 -0400

    youtube link

--- src/pages/AloeCam.js
+++ src/pages/AloeCam.js
@@ -1,6 +1,6 @@
 import "../styles.css"
 export default function AloeCam() {
-  return <iframe id="aloeCam" src="http://aloecam.ddns.net:6969/" width={1925} height={1090}></iframe>
+  return <iframe id="aloeCam" src="https://www.youtube.com/watch?v=AfowTsfJe50" width={1925} height={1090}></iframe>
   
 
 }
