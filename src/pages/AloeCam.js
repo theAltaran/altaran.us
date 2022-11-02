7e06267ab131fc162f910daa0b1596f485fb155a
Matt Craft <wmcraft1@gmail.com>
2022-11-02 12:55:47 -0400

    css changes

--- src/pages/AloeCam.js
+++ src/pages/AloeCam.js
@@ -1,6 +1,6 @@
 import "./AloeCam.css"
 export default function AloeCam() {
-  return <iframe title="aloecam" id="aloeCam" src="https://aloecam.ddns.net:6969" width={1925} height={1084} ></iframe>
+  return     <iframe title="aloecam" id="aloeCam" src="https://aloecam.ddns.net:6969" ></iframe>
   
 
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-import "../styles.css"
+import "./Home.css"
+
 export default function Home() {
-  return <div className="background">
+  return <div class="background">
     <h1>Sometimes I just do things, because the voices tell me to.</h1>
   </div> 
 }
