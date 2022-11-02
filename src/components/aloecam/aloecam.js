3071c1e5140f1c5de515c734b4846d54a3108dcd
Matt Craft <wmcraft1@gmail.com>
2022-11-02 13:37:09 -0400

    aloecam changes

--- src/components/aloecam/aloecam.js
+++ src/components/aloecam/aloecam.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from './aloecam.module.css';
 
 const Aloecam = () => (
-  <div className="aloecam">
+  <div className={styles.Aloecam}>
     <div>
     <h1>AloeCam</h1>
     <br/>
     <a href="http://aloecam.ddns.net:5000/motorOn">
-       <input type="button" value="Visit Page" />
+       <input type="button" value="Dont Click This" />
     </a>
     <br/>
-    <iframe title="aloecam" id="aloeCam" src="https://aloecam.ddns.net:6969" ></iframe>
+    <iframe title="aloecam" id="aloeCam" src="https://aloecam.ddns.net:6969" width="1920px" height="1080px"></iframe>
     </div>
   </div>
 );
