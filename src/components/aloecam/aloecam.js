201e75e8289c772171a6215da70dd02266c998dd
Matt Craft <wmcraft1@gmail.com>
2022-11-07 15:21:30 -0500

    aloecam changes

--- src/components/aloecam/aloecam.js
+++ src/components/aloecam/aloecam.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RandomButton from '../randomButton/randomButton';
-import RandomButton3 from '../randomButton3/randomButton3';
-import RandomButton4 from '../randomButton4/randomButton4';
+// import RandomButton3 from '../randomButton3/randomButton3';
+// import RandomButton4 from '../randomButton4/randomButton4';
 import styles from './aloecam.module.css';
 // import IframeResizer from 'iframe-resizer-react';
 
