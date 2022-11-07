f6d7f7e656cbb0433e13a5a4d63d0add73314f2a
Matt Craft <wmcraft1@gmail.com>
2022-11-07 15:16:52 -0500

    aloecam changes

--- src/components/randomButton/randomButton.js
+++ src/components/randomButton/randomButton.js
@@ -5,7 +5,7 @@ import startMotor1 from '../aloecam/startmotor1';
 const RandomButton = () => (
   <div className={styles.RandomButton}>
       <button className={styles.button1} onClick={startMotor1}>
-        1 MARBLE
+        Drop 1 Marble at a Time
         </button>
   </div>
 );
