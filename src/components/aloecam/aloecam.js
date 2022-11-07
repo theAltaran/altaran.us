5f1f064f3e068454d818cd71ce4998f62f57b196
Matt Craft <wmcraft1@gmail.com>
2022-11-07 08:40:24 -0500

    aloeCam to img

--- src/components/aloecam/aloecam.js
+++ src/components/aloecam/aloecam.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import RandomButton from '../randomButton/randomButton';
 import styles from './aloecam.module.css';
-import IframeResizer from 'iframe-resizer-react';
+// import IframeResizer from 'iframe-resizer-react';
 
 
 const Aloecam = () => (
   <div className={styles.Aloecam}>
     <div> 
     <br/>
-   <IframeResizer className={styles.iframe1} title="aloecam" src="https://aloecam.ddns.net:6969"></IframeResizer>
+   {/* <IframeResizer className={styles.iframe1} title="aloecam" src="https://aloecam.ddns.net:6969"></IframeResizer> */}
+   <img className={styles.img1} src="https://aloecam.ddns.net:6969"alt='aloeCam'></img>
     <br/>
        <RandomButton/>
     </div>
