54348412e5cd5ab434a660f9bd9b5c67bccc27e0
Matt Craft <wmcraft1@gmail.com>
2022-11-08 12:34:19 -0500

    css changes

--- src/components/aloecam/aloecam.js
+++ src/components/aloecam/aloecam.js
@@ -6,6 +6,8 @@ import Rules from '../../components/rules/rules'
 const Aloecam = () => (
   <div className={styles.Aloecam}>
     <br/>
+    <br/>
+    <br/>
    <h1>Welcome to the home of the aloeCam</h1>
    <img className={styles.img1} src="https://aloecam.ddns.net:6969"alt='aloeCam'></img>
     <br/>
