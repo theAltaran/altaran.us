2a6b0c417739936fc83aac77848ab5a0b0961a7d
Matt Craft <wmcraft1@gmail.com>
2022-11-08 18:45:22 -0500

    game

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './projects.module.css';
+import IframeResizer from 'iframe-resizer-react';
 
 const Projects = () => (
   <div className={styles.Projects}>
+    <br/>
+    <br/>
+    <br/>
 
+    <br/>
+    <br/>
+<IframeResizer className={styles.iframe1} src="https://nests-d3qhjnu95-altaran.vercel.app/"></IframeResizer>
     <br/>
   </div>
 );
