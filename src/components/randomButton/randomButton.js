ad74bedcec010cc5f80a463552d245ae040c4ec1
Matt Craft <wmcraft1@gmail.com>
2022-11-08 13:57:10 -0500

    doh

--- src/components/randomButton/randomButton.js
+++ src/components/randomButton/randomButton.js
@@ -2,12 +2,21 @@ import React from 'react';
 import styles from './randomButton.module.css';
 import startMotor1 from '../aloecam/startmotor1';
 
+function Sound1(url) {
+  new Audio(url).play();
+}
+function clickity() {
+  startMotor1();
+  Sound1('doh.mp3');
+}
+
 const RandomButton = () => (
   <div className={styles.RandomButton}>
-      <button className={styles.button1} onClick={startMotor1}>
+      <button className={styles.button1} onClick={clickity}>
         Drop It Like Its Hot
         </button>
   </div>
 );
 
 export default RandomButton;
+
