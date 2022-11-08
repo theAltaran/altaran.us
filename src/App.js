de63a3eaf27422785f070ec03b54c104a7e0948f
Matt Craft <wmcraft1@gmail.com>
2022-11-08 10:59:35 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AloeCam from "./components/aloecam/aloecam"
 import Home from "./components/home/home"
 import Projects from "./components/projects/projects"
 import { Route, Routes } from "react-router-dom"
-import Footer from "./components/footer/footer"
+// import Footer from "./components/footer/footer"
 import SmApp from "./components/smApp/smApp"
 
 
