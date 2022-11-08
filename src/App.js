3f9745390fcf39b32cd95fcc1e393aded7bd5ba8
Matt Craft <wmcraft1@gmail.com>
2022-11-08 15:51:33 -0500

    house cleaning

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import Projects from "./components/projects/projects"
 import { Route, Routes } from "react-router-dom"
 // import Footer from "./components/footer/footer"
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://dbAdmin:<password>@cluster0.gpjjcfh.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 function App() {
   return (
