1c3905e1979516e7ba85708f42b8f30f3020b224
Matt Craft <wmcraft1@gmail.com>
2022-11-08 20:37:55 -0500

    background img fixes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Route, Routes } from "react-router-dom"
 function App() {
   return (
     <>
+      <div className="everything">
       <Navbar />
       <div className="container">
         <Routes>
@@ -18,6 +19,7 @@ function App() {
         </Routes>
       </div>
       {/* <Footer /> */}
+      </div>
     </>
   )
 }
