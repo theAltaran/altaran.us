e391bfd89e7ed2195de1b6c75e60299202e0a978
Matt Craft <wmcraft1@gmail.com>
2022-11-12 13:07:03 -0500

    count

--- src/components/randomButton/clickity.js
+++ src/components/randomButton/clickity.js
@@ -4,6 +4,8 @@ import Sound1 from "./sound1";
 function clickity1() {
     startMotor1();
     Sound1('doh.mp3');
+    var ifr = document.getElementsByName('clickCounter')[0];
+    ifr.src = ifr.src;
   }
 
   export default clickity1;
