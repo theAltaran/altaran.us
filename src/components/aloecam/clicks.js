aa06c9457630a922a890e0269bc98af31f58088a
Matt Craft <wmcraft1@gmail.com>
2022-11-13 14:57:09 -0500

    stats

--- src/components/aloecam/clicks.js
+++ src/components/aloecam/clicks.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react"
 import styles from './aloecam.module.css';
 
-
-
-
 const Clicks = () => {
   const [clicks, setClicks] = useState([])
 
@@ -14,12 +11,15 @@ const Clicks = () => {
       })
       .then(data => {
         setClicks(data)
-      })
-  }
+      })}
 
   useEffect(() => {
     fetchData()
   }, [])
+
+  function refreshPage() {
+    window.location.reload(true);
+  }
 
   return (
     <div className={styles.clicks}>
@@ -28,7 +28,7 @@ const Clicks = () => {
           {clicks.map(user => (
             <p  key={user.id}>Drops to Date: {user.clicks}</p>
           ))}
-      
+      <button onClick={refreshPage}>Click to reload!</button>
         </div>
       )}
     </div>
--- src/components/randomButton/clickity.js
+++ src/components/randomButton/clickity.js
@@ -1,17 +1,8 @@
 import startMotor1 from "../aloecam/startmotor1";
 import Sound1 from "./sound1";
 
-// function refreshPage() {
-//   window.location.reload(true);
-// }
 function clickity1() {
     startMotor1();
-    Sound1('doh.mp3');
-    // refreshPage();
-    // var ifr = document.getElementsByName('clickCounter')[0];
-    // ifr.src = ifr.src;
-    // var ifr2 = document.getElementsByName('lastClickDate')[0];
-    // ifr2.src = ifr2.src;
-  }
+    Sound1('doh.mp3');  }
 
   export default clickity1;
