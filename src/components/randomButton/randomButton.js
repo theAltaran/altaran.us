ff60ba5be2bb20cf738cd6803647f14338b30692
Matt Craft <you@example.com>
2022-11-16 09:29:10 -0500

    apple fix(grrr apple3)

--- src/components/randomButton/randomButton.js
+++ src/components/randomButton/randomButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './randomButton.module.css';
-import clickity1 from './clickity';
+// import clickity1 from './clickity';
 import apple from './apple.png';
+import startMotor1 from '../aloecam/startmotor1';
+
 
 const RandomButton = () => (
   <div className={styles.RandomButton}>
-      <button className={styles.apple} onClick={clickity1}>
+      <button className={styles.apple} onClick={startMotor1}>
       <img src={apple} alt="Logo" className={styles.apple2} />
         </button>
         <br/>
