42fac4ee4c78d83aa115c1eff6478c4ead9b6531
Matt Craft <you@example.com>
2022-11-18 14:21:03 -0500

    nes instructions

--- src/components/alt/alt.js
+++ src/components/alt/alt.js
@@ -11,6 +11,20 @@ function hideNES() {
   }
 }
 
+function hideBreaks() {
+  var x = document.getElementById("weirdStuff");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function nesClick () {
+  hideNES();
+  hideBreaks();
+}
+
 function hideGoogle() {
   var x = document.getElementById("googleHide");
   if (x.style.display === "none") {
@@ -18,7 +32,6 @@ function hideGoogle() {
   } else {
     x.style.display = "none";
   }
-
 }
 
 const Alt = () => (
@@ -36,18 +49,42 @@ const Alt = () => (
     <br/>
     <u1 className={styles.list1}>...and things I enjoy</u1>
      <li className={styles.listItems}>Browser NES Emulator
-   <button className={styles.button1} onClick={hideNES}>Open/Close</button></li>
+   <button className={styles.button1} onClick={nesClick}>Open/Close</button></li>
   <div id="nesHide" className={styles.NES}>
    <IframeResizer  className={styles.iframe1} src="https://nests-d3qhjnu95-altaran.vercel.app/"></IframeResizer>
+   <br/>
+   <p className={styles.nesRom}>You need to supply your own NES rom.....Im not gonna help you pirate!</p>
+   <p className={styles.nesControls}><img src="nesControls.png" alt="alt" /></p>  
   </div>
+  <br/>
+  {/* Please ALt, find a better way to do this in the future.  This is darn right ugly. */}
+<div className={styles.Weirdstuff} id="weirdStuff">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    </div>
   <li className={styles.listItems}>thing 2</li>
      <li className={styles.listItems}>thing 3</li>
      <li className={styles.listItems}>thing 4</li>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+
   </div>
 );
 
