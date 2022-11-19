46b1b080c375361fb69bd455e1831fc16c8c14ee
Matt Craft <you@example.com>
2022-11-18 22:28:35 -0500

    lightsCameraAction

--- src/components/randomButton/clickity.js
+++ src/components/randomButton/clickity.js
@@ -3,9 +3,9 @@ import Sound1 from "./sound1";
 import lightsOn from "../aloecam/lightsOn";
 
 function clickity1() {
-    startMotor1();
     Sound1('doh.mp3');
     lightsOn();
+    startMotor1();
 
   }
   export default clickity1;
