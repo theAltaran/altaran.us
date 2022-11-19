52387390e99e1714a8d4f99078b0282500b89543
Matt Craft <you@example.com>
2022-11-19 13:44:57 -0500

    fix api address

--- src/components/aloecam/clicks.js
+++ src/components/aloecam/clicks.js
@@ -18,18 +18,7 @@ const Clicks = () => {
         setClicks(data)
       })}
 
-//   const fetchData = () => {
-//   fetch("https://api.altaran.us/clicks", {
-//   "headers": {
-//     "Authorization": `Basic ${btoa('apiUser:3Wp6S3ntRUcBUpT')}`,
-//   },
-// })
-//       .then(response => {
-//         return response.json()
-//       })
-//       .then(data => {
-//         setClicks(data)
-//       })}
+
 
   useEffect(() => {
     fetchData()
