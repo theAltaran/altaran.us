67d4c6dc3fac7ab2b1281ae13749fea93b44477c
Matt Craft <you@example.com>
2022-11-19 15:09:23 -0500

    css stuff

--- src/components/aloeInfo/aloeInfo.js
+++ src/components/aloeInfo/aloeInfo.js
@@ -5,7 +5,19 @@ import TopTenList from './topTenList';
 const AloeInfo = () => (
   <div>
   <div id="infoHide" className={styles.Rules}>
-    
+    <hr/>
+    <p className={styles.aloeInfo}>aloeInfo</p>
+   The counter will not update on your first click. 
+    <br/>
+    But it does update the database in the background, so 
+    <br/>
+    on your next click it will update by one.  
+
+    <br/>
+    <br/>
+    *tracking via IP for Now.  Will have more options in the future.
+    <br/>
+    <br/>
     <hr/>
     <p className={styles.RulesHeading}>Rules of the aloeCam</p>
     <br/>
@@ -15,13 +27,13 @@ const AloeInfo = () => (
     nightAloe feature should now be active
     <br/>
     <br/>
-    2. Marble Motor is on from 10AM EST to 10PM EST / 3PM to 3:00AM UTC
+    2. Marble Motor is on from 10AM EST to 10PM EST / 3PM to 3:00AM UTC**
     <br/>
     <br/>
-    3. Can't have marbles dropping while Fiona and I sleep now can I?
+    ** Can't have marbles dropping while Fiona and I sleep now can I?
     <br/>
     <br/>
-    *tracking via IP for Now.  Will have more options in the future.
+    
     
     <hr/>
     
