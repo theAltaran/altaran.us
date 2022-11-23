2b1b1f3ecbf441fdf66dd36627b64e603db370bf
Matt Craft <you@example.com>
2022-11-23 15:06:56 -0500

    video issues

--- src/components/aloecam/aloecam.js
+++ src/components/aloecam/aloecam.js
@@ -19,7 +19,7 @@ function hideMoreInfo() {
 const Aloecam = () => (
   <div className={styles.Aloecam}>
    <AloeCamFeed />
-   <h6>The aloeCam team is currently doing repairs.  Will be back soon. </h6>
+   <h6>The aloeCam team is currently doing repairs.  Will be back soon.  </h6>
     <Clicks />  
     <br/>
      
