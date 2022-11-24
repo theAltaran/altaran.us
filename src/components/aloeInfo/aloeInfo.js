309548967aca7539c09dec8c315ee2ce90d7a489
Matt Craft <you@example.com>
2022-11-24 11:59:48 -0500

    aloeInfo update

--- src/components/aloeInfo/aloeInfo.js
+++ src/components/aloeInfo/aloeInfo.js
@@ -7,32 +7,27 @@ const AloeInfo = () => (
   <div id="infoHide" className={styles.Rules}>
     <hr/>
     <p className={styles.aloeInfo}>aloeInfo</p>
+
+    Lights are on from 8AM EST to 10PM EST / 1PM to 3:00AM UTC
+    <br/>
+    nightAloe feature should now be active
+    <br/>
+    <br/>
+    Marble Motor is on from 10AM EST to 10PM EST / 3PM to 3:00AM UTC**
+    <br/>
+    <br/>
    The counter will not update on your first click. 
     <br/>
     But it does update the database in the background, so 
     <br/>
-    on your next click it will update by one.  
+    on your next click it will update by one.   
 
     <br/>
     <br/>
     *tracking via IP for Now.  Will have more options in the future.
     <br/>
-    <br/>
-    <hr/>
-    <p className={styles.RulesHeading}>Rules of the aloeCam</p>
-    <br/>
-    <br/>
-    1. Lights are on from 8AM EST to 10PM EST / 1PM to 3:00AM UTC
-    <br/>
-    nightAloe feature should now be active
-    <br/>
-    <br/>
-    2. Marble Motor is on from 10AM EST to 10PM EST / 3PM to 3:00AM UTC**
-    <br/>
-    <br/>
     ** Can't have marbles dropping while Fiona and I sleep now can I?
-    <br/>
-    <br/>
+
     
     
     <hr/>
