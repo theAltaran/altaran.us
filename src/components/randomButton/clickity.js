9bc3bf39c8715fa3430ffd11fc198af64c189ae0
Matt Craft <you@example.com>
2022-11-28 12:49:18 -0500

    mario

--- src/components/randomButton/clickity.js
+++ src/components/randomButton/clickity.js
@@ -2,7 +2,7 @@ import Sound1 from "./sound1";
 import lightsOn from "../aloecam/lightsOn";
 
 function clickity1() {
-    Sound1('doh.mp3');
+    Sound1('mario.mp3');
     startMotor1();
   }
   export default clickity1;
