f1affb0b156fc8cab9d2b6662fb23c5cd7167f9a
Matt Craft <you@example.com>
2023-01-02 13:47:09 -0500

    ai

--- src/components/altAI/altAI.js
+++ src/components/altAI/altAI.js
@@ -7,7 +7,7 @@ function ALTai() {
   const [answer, setAnswer] = useState("");
   const [enable, setEnable] = useState(false);
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   const handleSubmit = async() => {
