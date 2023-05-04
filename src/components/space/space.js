3f4d75533ccb21eb8ee09fded7cdf95963c0c460
Matt Craft <wmcraft1@gmail.com>
2023-05-03 21:20:54 -0400

    idk

--- src/components/space/space.js
+++ src/components/space/space.js
@@ -4,7 +4,7 @@ import RocketLaunchSchedule from './launches';
 // import SpaceXNews from './spaceXNews';
 
 import ReactGA from 'react-ga4';
-const TRACKING_ID = "G-1ZRM3378D2"; // OUR_TRACKING_ID
+const TRACKING_ID =  "G-1ZRM3378D2"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 const Space = () => (
