bfae7602a2f255099894ed23200583940a130a23
Matt Craft <wmcraft1@gmail.com>
2023-05-04 21:59:30 -0400

    new ddns

--- src/components/aloecam/aloeCamFeed.js
+++ src/components/aloecam/aloeCamFeed.js
@@ -8,8 +8,8 @@ const AloeCamFeed = () => (
       <br/>
       <br/>
     <h1>Welcome to the home of the aloeCam</h1>
-    {/* <img className={styles.img1} src="https://aloecam.ddns.net:6969"alt='aloeCam'></img> */}
-    <img className={styles.img1} src="https://aloecam.duckdns.org:6969"alt='aloeCam'></img>     
+    <img className={styles.img1} src="https://aloecam.ddns.net:6969"alt='aloeCam'></img>
+    {/* <img className={styles.img1} src="https://aloecam.duckdns.org:6969"alt='aloeCam'></img>      */}
       
       <br/>
       <br/>
