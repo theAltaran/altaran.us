bf2b6d7e5d0a2e1b20aaca14a6fbb2f2f9ad5b44
Matt Craft <wmcraft1@gmail.com>
2023-07-02 22:25:33 -0400

    liveStreams2

--- src/components/space/feed.js
+++ src/components/space/feed.js
@@ -3,7 +3,8 @@ import styles from './feed.module.css';
 
 const LiveStreamWall = () => {
   const [liveStreams, setLiveStreams] = useState([]);
-  const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const [channelTitles, setChannelTitles] = useState({});
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const channelIds = [
@@ -11,44 +12,69 @@ const LiveStreamWall = () => {
       'UCFwMITSkc1Fms6PoJoh1OUQ',
     ];
 
+    const fetchChannelTitle = async (channelId) => {
+      try {
+        const response = await fetch(`https://viewtube.altaran.duckdns.org/api/channels/${channelId}/livestreams?sort=sort`);
+        const data = await response.json();
+        const title = data.title;
+        setChannelTitles((prevChannelTitles) => ({
+          ...prevChannelTitles,
+          [channelId]: title,
+        }));
+      } catch (error) {
+        console.error(`Error fetching channel title for channel ${channelId}:`, error);
+      }
+    };
+
     const fetchLiveStreams = async () => {
       try {
         const promises = channelIds.map(async (channelId) => {
-          const response = await fetch(
-            `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&eventType=live&type=video&part=snippet`
-          );
+          await fetchChannelTitle(channelId);
+          const response = await fetch(`https://viewtube.altaran.duckdns.org/api/channels/${channelId}/livestreams?sort=sort`);
           const data = await response.json();
-          return data.items;
+          return data;
         });
 
         const streams = await Promise.all(promises);
         const flattenedStreams = streams.flat();
         setLiveStreams(flattenedStreams);
+        setError(false); // Reset the error state on successful fetch
       } catch (error) {
+        setError(true);
         console.error('Error fetching live streams:', error);
       }
     };
 
     fetchLiveStreams();
-  }, [apiKey]);
+  }, []);
 
   return (
     <div className={styles.container}>
-      {liveStreams &&
-        liveStreams.map((stream) => (
-          <div key={stream.id.videoId} className={styles.videoContainer}>
-            <a
-              href={`https://viewtube.altaran.duckdns.org/watch?v=${stream.id.videoId}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={stream.snippet.thumbnails.medium.url} alt={stream.snippet.title} className={styles.videoImage} />
-            </a>
-          </div>
-        ))}
-      {!liveStreams || liveStreams.length === 0 ? (
+      {Object.keys(channelTitles).map((channelId) => (
+        <div key={channelId}>
+          <h2>Channel: {channelTitles[channelId]}</h2>
+          {liveStreams
+            .filter((stream) => stream?.authorId === channelId && stream?.publishedText === 'Live')
+            .map((stream) => (
+              <div key={stream.videoId} className={styles.videoContainer}>
+                <a
+                  href={`https://viewtube.altaran.duckdns.org/watch?v=${stream.videoId}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={stream.videoThumbnails[0]?.url}
+                    alt={stream.title}
+                    className={styles.videoImage}
+                  />
+                </a>
+              </div>
+            ))}
+        </div>
+      ))}
+      {!error && (!liveStreams || liveStreams.length === 0) && (
         <div>No live streams available</div>
-      ) : null}
+      )}
     </div>
   );
 };
