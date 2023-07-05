cef3331d5c532d773bd13b8f5ba00f623191f31f
Matt Craft <wmcraft1@gmail.com>
2023-07-05 11:42:54 -0400

    headings

--- src/components/space/space.js
+++ src/components/space/space.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import styles from './space.module.css';
 import LiveStreamWall from './feed';
-import RocketLaunchSchedule from "./launches";
+import RocketLaunchSchedule from "./launches"
 // import SpaceXNews from './spaceXNews';
 import StarShipStatus from './starShipStatus';
 
 const Space = () => (
-  <div>
+  <div className={styles.space}>
+    {/* <SpaceXNews /> */}
+    <div className={styles.starShipStatusContainer}>
+      <StarShipStatus />
+    </div>
     <h1>Upcoming Launch Schedule</h1>
     <RocketLaunchSchedule />
     <br />
     <br />
-    <div className={styles.space}>
-      <div className={styles.starShipStatusContainer}>
-        <StarShipStatus />
-      </div>
-    </div>
     <h1>Live Streams</h1>
     <div className={styles.videoBox}>
       <LiveStreamWall />
