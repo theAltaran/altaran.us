8d4459096e6e53ee0a0e1d72b66cc5b671517f8f
Matt Craft <wmcraft1@gmail.com>
2023-08-02 19:42:13 -0400

    bobsDrunk

--- src/components/bobsnews/bobsnews.js
+++ src/components/bobsnews/bobsnews.js
@@ -20,7 +20,7 @@ const BobsNews = () => {
   const handleGenerateContent = () => {
     setLoading(true);
 
-    axios.post('https://wp.altaran.duckdns.org/generate-content', {
+    axios.post('https://bobsnews.altaran.us/generate-content', {
       category: category,
       starter_info: starterInfo,
     })
