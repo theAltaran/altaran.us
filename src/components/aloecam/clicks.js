1c68c4409a76eb68645eae9a41dd8bc9ced42baa
Matt Craft <wmcraft1@gmail.com>
2023-12-02 12:05:19 -0500

    doh

--- src/components/aloecam/clicks.js
+++ src/components/aloecam/clicks.js
@@ -30,8 +30,8 @@ const Clicks = () => {
     // Sound1('hohoho.mp3')
     // Sound1('HappyNewYear.mp3')
     // Sound1('haha.mp3');
-    // Sound1('doh.mp3');
-    Sound1('chunky.mp3');
+    Sound1('doh.mp3');
+    // Sound1('chunky.mp3');
     // Sound1('mario.mp3');
     // Sound1('idiots.mp3');
     // Sound1('swimming.mp3')
