835c23d6f7579dcc16b63d612bc1191e0ed4a6d0
Matt Craft <wmcraft1@gmail.com>
2024-08-15 09:56:11 -0400

    added timeout

--- src/components/aloecam/aloeCamFeed.js
+++ src/components/aloecam/aloeCamFeed.js
@@ -8,11 +8,8 @@ const AloeCamFeed = () => (
       <br/>
       <br/>
     <h1>Welcome to the home of the aloeCam</h1>
-    {/* <h3 className={styles.center}>**5-7-23 Currently making changes to dns.  aloeCam functionality</h3>
-    <h3 className={styles.center}>maybe limited for the next 24-48 hours. Thank you for your cooperation**</h3> */}
     <img className={styles.img1} src="https://aloecam.altaran.us/"alt='aloeCam'></img>
-    {/* <img className={styles.img1} src="https://cam.aloecam.duckdns.org/"alt='aloeCam'></img> */}
-    {/* <img className={styles.img1} src="https://aloecam.duckdns.org:6969"alt='aloeCam'></img>      */}
+
       
       <br/>
       <br/>
--- src/components/aloecam/clicks.js
+++ src/components/aloecam/clicks.js
@@ -1,30 +1,27 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import styles from './aloecam.module.css';
 import startMotor1 from "../aloecam/startmotor1";
 import Sound1 from "../randomButton/sound1";
 // import lightsOn from "./lightsOn";
 
-
-
 const Clicks = () => {
-  const [clicks, setClicks] = useState([])
+  const [clicks, setClicks] = useState([]);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // State to track if the button is disabled
 
   const fetchData = () => {
     fetch("http://api.altaran.duckdns.org/clicks")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setClicks(data)
-      })}
-
-
+      .then((response) => response.json())
+      .then((data) => setClicks(data));
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   function clickity2() {
+    if (isButtonDisabled) return; // Prevents function execution if button is disabled
+
+    setIsButtonDisabled(true); // Disable the button
     // lightsOn();
     startMotor1();
     // Sound1('hohoho.mp3')
@@ -36,21 +33,33 @@ const Clicks = () => {
     // Sound1('idiots.mp3');
     // Sound1('swimming.mp3')
     fetchData();
-    
-}
+
+    // Re-enable the button after 3 seconds
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 3000);
+  }
 
   return (
     <div className={styles.clicks}>
       {clicks.length > 0 && (
         <div>
-          {clicks.map(user => (
-            <p className={styles.dropCount}>  <button className={styles.button1} onClick={clickity2}> Drop it LIke Its Hot</button>
-            {user.id}Drops to Date: {user.clicks}</p>
+          {clicks.map((user) => (
+            <p className={styles.dropCount} key={user.id}>
+              <button
+                className={styles.button1}
+                onClick={clickity2}
+                disabled={isButtonDisabled} // Disables the button when state is true
+              >
+                Drop it Like Its Hot
+              </button>
+              Drops to Date: {user.clicks}
+            </p>
           ))}
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Clicks
+export default Clicks;
